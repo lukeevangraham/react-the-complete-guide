becefb71c58a4061bfa467bb9c441d42da8bd5aa
Cefalu Wave <luke.evan.graham@gmail.com>
2020-10-02 22:47:18 -0700

    completed the hook intro

---
+++ 1-first-project/src/App.js
@@ -0,0 +1,49 @@
+import React from "react";
+import Person from "./Person/Person";
+import "./App.css";
+
+class App extends React.Component {
+  state = {
+    persons: [
+      { name: "Max", age: 28 },
+      { name: "Manu", age: 29 },
+      { name: "Stephanie", age: 26 },
+    ],
+  };
+
+  switchNameHandler = () => {
+    // console.log("Was clicked!")
+    this.setState({
+      persons: [
+        { name: "Maximilian", age: 28 },
+        { name: "Manu", age: 29 },
+        { name: "Stephanie", age: 27 },
+      ],
+    });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Hi, I'm a React App</h1>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}
+        />
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+        />
+        <Person
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age}
+        >
+          My Hobbies: Racing
+        </Person>
+      </div>
+    );
+  }
+}
+
+export default App;
