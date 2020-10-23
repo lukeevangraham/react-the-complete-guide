d2d4a95e1c7727892c44c8a31c8b405288995c00
Cefalu Wave <luke.evan.graham@gmail.com>
2020-10-22 22:10:13 -0700

    working on lazy loading routes

---
+++ 6-lazy-react-intro/src/App.js
@@ -0,0 +1,36 @@
+import React, { Component, Suspense } from "react";
+import { BrowserRouter, Route, NavLink } from "react-router-dom";
+
+import User from "./containers/User";
+import Welcome from "./containers/Welcome";
+
+const Posts = React.lazy(() => import("./containers/Posts"));
+
+class App extends Component {
+  state = { showPosts: false };
+
+  modeHandler = () => {
+    this.setState((prevState) => {
+      return { showPosts: !prevState.showPosts };
+    });
+  };
+
+  render() {
+    return (
+      (
+        <React.Fragment>
+          <button onClick={this.modeHandler}>Toggle Mode</button>
+          {this.state.showPosts ? (
+            <Suspense fallback={<div>Loading...</div>}>
+              <Posts />
+            </Suspense>
+          ) : (
+            <User />
+          )}
+        </React.Fragment>
+      )
+    );
+  }
+}
+
+export default App;
