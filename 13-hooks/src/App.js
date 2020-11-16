789ffae171885f06df7d8a80a5cdd7a930edaa1a
Cefalu Wave <luke.evan.graham@gmail.com>
2020-11-15 22:57:21 -0800

    dealing with a v17 issue

--- 13-hooks/src/App.js
+++ 13-hooks/src/App.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useContext } from "react";
+import { jsx as _jsx } from 'react/jsx-runtime'
 
-import Ingredients from './components/Ingredients/Ingredients';
+import Ingredients from "./components/Ingredients/Ingredients";
+import Auth from "./components/Auth";
+import { AuthContext } from "./context/auth-context";
 
-const App = props => {
-  return <Ingredients />;
+const App = (props) => {
+  const authContext = useContext(AuthContext)
+
+  let content = <Auth />
+  if(authContext.isAuth) {
+    content = <Ingredients />
+  }
+
+  return _jsx(
+    <AuthContext.Consumer>
+      <Auth />
+    </AuthContext.Consumer>
+  );
 };
 
 export default App;
