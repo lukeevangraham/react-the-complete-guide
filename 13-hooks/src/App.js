845a271b26dc7e7d302bbace2e708dd657c7d72c
Cefalu Wave <luke.evan.graham@gmail.com>
2020-11-15 23:11:37 -0800

    correct the return in app

--- 13-hooks/src/App.js
+++ 13-hooks/src/App.js
@@ -1,22 +1,21 @@
 import React, { useContext } from "react";
-import { jsx as _jsx } from 'react/jsx-runtime'
+// import { jsx as _jsx } from 'react/jsx-runtime'
 
 import Ingredients from "./components/Ingredients/Ingredients";
 import Auth from "./components/Auth";
 import { AuthContext } from "./context/auth-context";
 
 const App = (props) => {
-  const authContext = useContext(AuthContext)
+  const authContext = useContext(AuthContext);
 
-  let content = <Auth />
-  if(authContext.isAuth) {
-    content = <Ingredients />
+  let content = <Auth />;
+
+  if (authContext.isAuth) {
+    content = <Ingredients />;
   }
 
-  return _jsx(
-    <AuthContext.Consumer>
-      <Auth />
-    </AuthContext.Consumer>
+  return (
+content
   );
 };
 
